6bc6f5caa4715fed3bca381b48eec676ba21d550
unknown <martinmbungunjoki@gmail.com>
2026-08-18 19:48:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# it has to have a route.
+# it has to have a method(POST/GET/PUT).
+# It has to have a status code(200, 201, 403).
+# It has to return data as JSON(key:value pairs).
+
+from flask import Flask, request, jsonify
+import json
+
+app =Flask(__name__)
+
+@app.route("/")
+def home():
+    if request.method == 'GET':
+        data = {"Flask API" : "Version 1"}
+        return jsonify(data), 200
+    else:
+        error = {"Error" : "Method not allowed"}
+        return jsonify(error), 403
+
+app.run(debug=True)    
